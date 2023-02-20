5ec2d572b77046a941bc9837c197007c14dc9c9f
nohago <125481989+nohago@users.noreply.github.com>
2023-02-20 11:07:42 -0500

    Add files via upload
    updated craps program

--- M3LAB3/main.cpp
+++ M3LAB3/main.cpp
@@ -1,67 +1,79 @@
 // CSC134
 // M3LAB3
-// noha goumaa
+// Noha Goumaa
 // 2/15/23
 
 #include <iostream>
 #include <cstdlib>
-#include <ctime> // enables use of time
+#include <ctime> // use of time
 using namespace std;
 
-// this program simlates a songle game of crps.
-// first version (MVP) is a single roll. 
+// this program simlates a single game of crps.
 
 int main() {
-  int die1, die2, roll; //2d6
-  int seed; // will use time(0) later
-  // char any_key; 
-  
-  cout << "Welcome to the craps table!" << endl;
 
-  //for testing, we will usee a repreatable seed
-  cout << "What's your lucky number? "; 
-  cin >> seed;
+  // variables
+  int die1, die2, roll;   // roll is 2d6
+  int seed;               // will use time(0) later
+  //char any_key;           // for cin.get()
+
+  
+  bool winner = false;
+  cout << "Welcome to the craps table!\n";
+
+  // cout << "What's your lucky number? ";
+  // cin >> seed;
+  // seed is truly random
+  seed = time(0);
   srand(seed);
   
   // start game
-  cout << "Let's roll! " << endl; //TODO: "press any key"
-  die1 = (rand() % 6) + 1;
+  cout << "Let's roll! " << endl; 
+  die1 = (rand() % 6) + 1;   // from 1 to 6
   die2 = (rand() % 6) + 1;
-  roll = die1 +die2;
-  
+  roll = die1 + die2;
+  // DEBUG: just use lucky number as roll
+  // roll = seed; // take this out!
+
   cout << "You rolled: (" << die1 << " + " << die2 << ") -> ";
   cout << roll << endl;
-  
-// determine outcome!
-switch (roll) {
-  // instant losses
-  case 2:
-    cout << "Snake eyes!" << endl;
-    winner = false;
-    break;
 
-  case 12:
-    cout << "Boxcars!" << endl;
-    winner = false;
-    break; 
-}
-  // instant wins
+  // determine outcome!
+  switch (roll) {
+    //  instant losses
+    case 2:
+      cout << "Two - Snake eyes!" << endl;
+      winner = false;
+      break;
+    case 12:
+      cout << "Twelve! Boxcars!" << endl;
+      winner = false;
+      break;
 
-  // to be ocntinues (point)
-  default:
-    cout << "UNKNOWN - TODO" << endl;
-
-  } 
+    // instant wins
+    case 7: 
+      cout << "Lucky number seven!" << endl;
+      winner = true;
+      break;
+    case 11:
+      cout << "Eleven! Eleven's a winner!" << endl;
+      winner = true;
+      break;
+    
+    // point rolls
+    default:
+      cout << "Rolled: " << roll << endl;
+      cout << "Continue playing, point is " << roll << endl;
+      cout << "But we'll just call it a draw." << endl;
+      break;
+  }
   if (winner) {
     cout << "You won!" << endl;
   } else {
-    cout << "Sorry -- you didnt win" << endl;
+    cout << "Sorry -- you didn't win" << endl;
   }
-
-  
   // end game
-  cout << "Thanks for playing!" << endl;
-  
+  cout << "Thank you for playing!" << endl;
   return 0;
   
 }
