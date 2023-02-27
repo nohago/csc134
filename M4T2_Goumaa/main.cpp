36a9e23812fcc70d794aafc00e5e1d987fa23f55
nohago <goumaan3791@student.faytechcc.edu>
2023-02-27 11:41:46 -0500

    updated M4T2 adding while and for loop

--- M4T2_Goumaa/main.cpp
+++ M4T2_Goumaa/main.cpp
@@ -6,9 +6,57 @@ using namespace std;
 // Noha Goumaa
 // 2/26/2023
 
+//menu functions
+void option_one() {
+    // count 0-9
+    cout << "Using while() to count" << endl;
+    int num = 0;
+    while (num < 10) {
+        cout << num << " ";
+        num ++;
+    }
+    cout << endl;
+
+    cout << "Using for() to count" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+void option_two() {
+    // sum up 5 numbers
+}
+
+void option_three() {
+    // TODO
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    // display a menu
+    // user choose 1,2,3
+    cout << "Welcome to the menu" << endl;
+    cout << "Press 1 to count" << endl;
+    cout << "Press 2 to sum up numbers" << endl;
+    cout << "Press 3 to TODO" << endl; // not implemented yet
+    cout << "Choice: ";
+
+    // input validation
+    int choice;
+    cin >> choice;
+    // user must pick 1,2,3
+     while (choice < 1 || choice > 3) {
+        cout << "Please choose 1, 2, or 3: ";
+        cin >> choice;
+    }
+    // if we get here, we know choice is invalid
+    if (choice == 1) {
+        option_one(); // function call
+    }
+
+    cout << "Thank you for using the menu!"<< endl;
+
     // Print three line menu
     // Press 1 to blah etc
     // cin an integer
