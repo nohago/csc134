1fe9c3b07fe47e9ce3869b4d4b0bed7c7ffd9589
nohago <goumaan3791@student.faytechcc.edu>
2023-02-27 11:11:02 -0500

    updated M4T2

---
+++ M4T2_Goumaa/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+// CSC 134
+// M4T2 - menus
+// Noha Goumaa
+// 2/26/2023
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    // Print three line menu
+    // Press 1 to blah etc
+    // cin an integer
+
+
+
+    return 0;
+}
